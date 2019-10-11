8ea776d395704491f5e4202afb383e3048b6d8f8
kimcu-on-thenet <kim.cuhoang@gmail.com>
2019-10-11 15:30:11 +0700

    Add unittest for CatalogCategory

---
+++ source/productcatalog/test/DDDEfCore.ProductCatalog.Core.DomainModels.Tests/TestCatalogCategory/TestCatalogCategoryModification.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoFixture.Xunit2;
+using DDDEfCore.Core.Common.Models;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Catalogs;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Categories;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Exceptions;
+using Shouldly;
+using Xunit;
+
+namespace DDDEfCore.ProductCatalog.Core.DomainModels.Tests.TestCatalogCategory
+{
+    public class TestCatalogCategoryModification
+    {
+        [Theory(DisplayName = "Change Display Name Successfully")]
+        [AutoData]
+        public void ChangeDisplayNameSuccessfully(string aDisplayName)
+        {
+            var categoryId = IdentityFactory.Create<CategoryId>();
+            var catalogId = IdentityFactory.Create<CatalogId>();
+            
+            var catalogCategory = CatalogCategory
+                                    .Create(catalogId, categoryId)
+                                    .WithDisplayName(aDisplayName);
+            
+            catalogCategory.DisplayName.ShouldBe(aDisplayName);
+        }
+
+        [Fact(DisplayName = "Change Display Name To Empty Should Throw Exception ")]
+        public void ChangeDisplayNameToEmptyShouldThrowException()
+        {
+            var categoryId = IdentityFactory.Create<CategoryId>();
+            var catalogId = IdentityFactory.Create<CatalogId>();
+
+            var catalogCategory = CatalogCategory
+                .Create(catalogId, categoryId);
+
+            Should.Throw<DomainException>(() => catalogCategory.WithDisplayName(string.Empty));
+        }
+    }
+}
