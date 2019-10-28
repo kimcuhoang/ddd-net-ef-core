dc36d1929e768300e1e09f068e38d3940b8b07f8
kimcu-on-thenet <kim.cuhoang@gmail.com>
2019-10-28 17:12:30 +0700

    Add GetCatalogCategoryDetail Query - Handler -Validation - Integration Test (not complete)

---
+++ source/productcatalog/test/DDDEfCore.ProductCatalog.Services.Queries.Tests/TestCatalogCategoryQueries/TestCatalogCategoryFixture.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using DDDEfCore.Core.Common.Models;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Catalogs;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Categories;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Products;
+using GenFu;
+using Xunit;
+
+namespace DDDEfCore.ProductCatalog.Services.Queries.Tests.TestCatalogCategoryQueries
+{
+    [Collection(nameof(Tests.SharedFixture))]
+    public class TestCatalogCategoryFixture : BaseTestFixture<Catalog>, IAsyncLifetime
+    {
+        public TestCatalogCategoryFixture(SharedFixture sharedFixture) : base(sharedFixture)
+        {
+        }
+
+        #region Implementation of IAsyncLifetime
+
+        public Catalog Catalog { get; private set; }
+
+        public CatalogCategory CatalogCategory => this.Catalog.Categories.FirstOrDefault();
+
+        public IEnumerable<CatalogProduct> CatalogProducts => this.CatalogCategory.Products;
+
+        public async Task InitializeAsync()
+        {
+            this.Catalog = this.CreateCatalog();
+            await this.SharedFixture.SeedingData(this.Catalog);
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+
+        #endregion
+
+        private Catalog CreateCatalog()
+        {
+            var catalog = Catalog.Create(this.Fixture.Create<string>());
+            var categoryId = IdentityFactory.Create<CategoryId>();
+            var catalogCategory = catalog.AddCategory(categoryId, this.Fixture.Create<string>());
+            
+            var totalCatalogProducts = A.Random.Next(1, 5);
+            Enumerable.Range(0, totalCatalogProducts).ToList().ForEach(idx =>
+            {
+                var productId = IdentityFactory.Create<ProductId>();
+                catalogCategory.CreateCatalogProduct(productId, this.Fixture.Create<string>());
+            });
+
+            return catalog;
+        }
+    }
+}
---
+++ source/productcatalog/test/DDDEfCore.ProductCatalog.Services.Queries.Tests/TestCatalogCategoryQueries/TestGetCatalogCategoryDetail.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Catalogs;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Products;
+using DDDEfCore.ProductCatalog.Services.Queries.CatalogCategoryQueries.GetCatalogCategoryDetail;
+using MediatR;
+using Shouldly;
+using Xunit;
+
+namespace DDDEfCore.ProductCatalog.Services.Queries.Tests.TestCatalogCategoryQueries
+{
+    [Collection(nameof(SharedFixture))]
+    public class TestGetCatalogCategoryDetail : IClassFixture<TestCatalogCategoryFixture>
+    {
+        private readonly TestCatalogCategoryFixture _testFixture;
+        private readonly GetCatalogCategoryDetailRequestValidator _validator;
+        private readonly CancellationToken _cancellationToken;
+
+        public TestGetCatalogCategoryDetail(TestCatalogCategoryFixture testFixture)
+        {
+            this._testFixture = testFixture ?? throw new ArgumentNullException(nameof(testFixture));
+            this._validator = new GetCatalogCategoryDetailRequestValidator();
+            this._cancellationToken = new CancellationToken(false);
+        }
+
+        private async Task DoAction(GetCatalogCategoryDetailRequest request, Action<GetCatalogCategoryDetailResult> assertAction)
+        {
+            await this._testFixture.ExecuteScopeAsync(async dbConnectionFactory =>
+            {
+                IRequestHandler<GetCatalogCategoryDetailRequest, GetCatalogCategoryDetailResult> requestHandler =
+                    new RequestHandler(dbConnectionFactory, this._validator);
+
+                var result = await requestHandler.Handle(request, this._cancellationToken);
+
+                assertAction(result);
+            });
+        }
+
+        [Theory(DisplayName = "Should GetCatalogCategoryDetail With Paging CatalogProduct Correctly")]
+        [InlineData(0, 0)]
+        [InlineData(1, 0)]
+        [InlineData(1, int.MaxValue)]
+        [InlineData(int.MaxValue, int.MaxValue)]
+        public async Task Should_GetCatalogCategoryDetail_With_Paging_CatalogProduct_Correctly(int pageIndex, int pageSize)
+        {
+            var catalogCategory = this._testFixture.CatalogCategory;
+            var catalogCategoryId = catalogCategory.CatalogCategoryId.Id;
+            var request = new GetCatalogCategoryDetailRequest(catalogCategoryId)
+            {
+                CatalogProductCriteria = new GetCatalogCategoryDetailRequest.CatalogProductSearchRequest
+                {
+                    PageIndex = pageIndex,
+                    PageSize = pageSize
+                }
+            };
+
+            await this.DoAction(request, result =>
+            {
+                result.ShouldNotBeNull();
+                result.CatalogCategoryDetail.ShouldNotBeNull();
+                result.CatalogCategoryDetail.CatalogId.ShouldBe(this._testFixture.Catalog.CatalogId.Id);
+                result.CatalogCategoryDetail.CatalogCategoryId.ShouldBe(catalogCategoryId);
+                result.CatalogCategoryDetail.CatalogCategoryName.ShouldBe(this._testFixture.CatalogCategory.DisplayName);
+                result.CatalogCategoryDetail.CatalogName.ShouldBe(this._testFixture.Catalog.DisplayName);
+
+                result.TotalOfCatalogProducts.ShouldBe(catalogCategory.Products.Count());
+
+                result.CatalogProducts.ToList().ForEach(c =>
+                {
+                    var catalogProduct = catalogCategory.Products.SingleOrDefault(x =>
+                        x.CatalogProductId == new CatalogProductId(c.CatalogProductId));
+
+                    catalogProduct.ShouldNotBeNull();
+                    c.DisplayName.ShouldBe(catalogProduct.DisplayName);
+                    c.ProductId.ShouldBe(catalogProduct.CatalogProductId.Id);
+                });
+            });
+        }
+    }
+}
