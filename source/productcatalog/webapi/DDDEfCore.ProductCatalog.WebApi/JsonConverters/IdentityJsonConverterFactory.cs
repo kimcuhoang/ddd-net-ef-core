702d36a3afa2bfecf8bc6c97d9b783f7221e1bb7
kimcu-on-thenet <kim.cuhoang@gmail.com>
2019-11-12 15:28:43 +0700

    Using System.Text.Json in .NET Core 3.0 - Convert Guid to concrete type of IdentityBase

---
+++ source/productcatalog/webapi/DDDEfCore.ProductCatalog.WebApi/JsonConverters/IdentityJsonConverterFactory.cs
@@ -0,0 +1,23 @@
+﻿using DDDEfCore.Core.Common.Models;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DDDEfCore.ProductCatalog.WebApi.JsonConverters
+{
+    public class IdentityJsonConverterFactory : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return !typeToConvert.IsGenericType 
+                    && typeToConvert.BaseType != null && typeToConvert.BaseType == typeof(IdentityBase);
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            var converterType = typeof(IdentityJsonConverter<>).MakeGenericType(typeToConvert);
+
+            return (JsonConverter)Activator.CreateInstance(converterType);
+        }
+    }
+}
--- source/productcatalog/webapi/DDDEfCore.ProductCatalog.WebApi/Startup.cs
+++ source/productcatalog/webapi/DDDEfCore.ProductCatalog.WebApi/Startup.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using DDDEfCore.ProductCatalog.Services.Commands;
 using DDDEfCore.ProductCatalog.Services.Queries;
 using DDDEfCore.ProductCatalog.WebApi.Infrastructures;
+using DDDEfCore.ProductCatalog.WebApi.JsonConverters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -29,21 +30,9 @@ namespace DDDEfCore.ProductCatalog.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers()
-                .ConfigureApiBehaviorOptions(options =>
+                .AddJsonOptions(configure =>
                 {
-                    options.InvalidModelStateResponseFactory = context =>
-                    {
-                        var errors = context.ModelState.Values
-                                .SelectMany(x => x.Errors.Select(p => p.ErrorMessage))
-                                .ToList();
-                        var result = new
-                        {
-                            Code = HttpStatusCode.BadRequest,
-                            Message = "Validation errors",
-                            Errors = errors
-                        };
-                        return new BadRequestObjectResult(result);
-                    };
+                    configure.JsonSerializerOptions.Converters.Add(new IdentityJsonConverterFactory());
                 });
             services.AddSingleton<IConfiguration>(sp => this.Configuration);
             services.AddApplicationCommands();
@@ -103,4 +92,7 @@ namespace DDDEfCore.ProductCatalog.WebApi
  *      => V1 must be matched
  *  3. GlobalExceptionMiddleware
  *      https://code-maze.com/global-error-handling-aspnetcore/
+ *  4. Using System.Text.Json in .NET Core 3.0
+ *      - https://andrewlock.net/using-strongly-typed-entity-ids-to-avoid-primitive-obsession-part-2/
+ *      - https://weblogs.thinktecture.com/pawel/2019/10/aspnet-core-3-0-custom-jsonconverter-for-the-new-system_text_json.html
  */
