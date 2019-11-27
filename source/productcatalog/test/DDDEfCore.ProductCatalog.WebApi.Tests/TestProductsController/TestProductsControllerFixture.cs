c92174aef19e95a3682bc04c5b176baacb826288
kimcu-on-thenet <kim.cuhoang@gmail.com>
2019-11-27 08:54:00 +0700

    Test CreateProduct Web Api

---
+++ source/productcatalog/test/DDDEfCore.ProductCatalog.WebApi.Tests/TestProductsController/TestProductsControllerFixture.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Products;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DDDEfCore.ProductCatalog.WebApi.Tests.TestProductsController
+{
+    [Collection(nameof(SharedFixture))]
+    public class TestProductsControllerFixture : SharedFixture
+    {
+        public string BaseUrl => $"api/products";
+        public Product Product { get; private set; }
+
+        #region Overrides of SharedFixture
+
+        public override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            this.Product = Product.Create(this.AutoFixture.Create<string>());
+            await this.SeedingData(this.Product);
+        }
+
+        #endregion
+    }
+}
