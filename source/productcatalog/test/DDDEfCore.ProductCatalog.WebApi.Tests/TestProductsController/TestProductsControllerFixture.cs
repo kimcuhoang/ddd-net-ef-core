8d4d80cdb07e6610cf8265bc8c49783fd638e73a
kimcu-on-thenet <kim.cuhoang@gmail.com>
2019-11-27 10:21:47 +0700

    Test GetProductDetail Web Api

--- source/productcatalog/test/DDDEfCore.ProductCatalog.WebApi.Tests/TestProductsController/TestProductsControllerFixture.cs
+++ source/productcatalog/test/DDDEfCore.ProductCatalog.WebApi.Tests/TestProductsController/TestProductsControllerFixture.cs
@@ -1,6 +1,8 @@
 ﻿using AutoFixture;
 using DDDEfCore.ProductCatalog.Core.DomainModels.Products;
 using System.Threading.Tasks;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Catalogs;
+using DDDEfCore.ProductCatalog.Core.DomainModels.Categories;
 using Xunit;
 
 namespace DDDEfCore.ProductCatalog.WebApi.Tests.TestProductsController
@@ -10,6 +12,10 @@ namespace DDDEfCore.ProductCatalog.WebApi.Tests.TestProductsController
     {
         public string BaseUrl => $"api/products";
         public Product Product { get; private set; }
+        public Category Category { get; private set; }
+        public Catalog Catalog { get; private set; }
+        public CatalogCategory CatalogCategory { get; private set; }
+        public CatalogProduct CatalogProduct { get; private set; }
 
         #region Overrides of SharedFixture
 
@@ -19,6 +25,14 @@ namespace DDDEfCore.ProductCatalog.WebApi.Tests.TestProductsController
 
             this.Product = Product.Create(this.AutoFixture.Create<string>());
             await this.SeedingData(this.Product);
+
+            this.Category = Category.Create(this.AutoFixture.Create<string>());
+            await this.SeedingData(this.Category);
+
+            this.Catalog = Catalog.Create(this.AutoFixture.Create<string>());
+            this.CatalogCategory = this.Catalog.AddCategory(this.Category.CategoryId, this.Category.DisplayName);
+            this.CatalogProduct = this.CatalogCategory.CreateCatalogProduct(this.Product.ProductId, this.Product.Name);
+            await this.SeedingData(this.Catalog);
         }
 
         #endregion
