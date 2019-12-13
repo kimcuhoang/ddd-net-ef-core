6c86f5114eaea521e89acab8fcdac78a25a0e1dd
kimcu-on-thenet <kim.cuhoang@gmail.com>
2019-12-13 09:03:05 +0700

    Using GenericHost for integration test

--- source/productcatalog/test/DDDEfCore.ProductCatalog.WebApi.Tests/SharedFixture.cs
+++ source/productcatalog/test/DDDEfCore.ProductCatalog.WebApi.Tests/SharedFixture.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Data;
+﻿using AutoFixture;
+using DDDEfCore.Core.Common.Models;
 using DDDEfCore.Infrastructures.EfCore.Common.Migration;
 using DDDEfCore.ProductCatalog.WebApi.Infrastructures.JsonConverters;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Respawn;
+using System;
+using System.Data;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
-using AutoFixture;
-using DDDEfCore.Core.Common.Models;
-using Microsoft.EntityFrameworkCore.Internal;
 using Xunit;
 
 namespace DDDEfCore.ProductCatalog.WebApi.Tests
@@ -23,22 +25,27 @@ namespace DDDEfCore.ProductCatalog.WebApi.Tests
     {
         private readonly Checkpoint _checkpoint;
         private readonly IServiceScopeFactory _serviceScopeFactory;
-        private readonly TestServer _testServer;
         private readonly JsonSerializerOptions _jsonSerializerOptions;
         protected readonly IFixture AutoFixture;
+        private readonly IHost _host;
 
         public SharedFixture()
         {
-            var webHostBuilder = new WebHostBuilder()
-                .ConfigureAppConfiguration((webHostBuilderContext, configurationBuilder) =>
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHost =>
+                {
+                    webHost.UseTestServer();
+                    webHost.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((hostBuilderContext, configurationBuilder) =>
                 {
                     configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
                     configurationBuilder.AddJsonFile("appsettings.json");
-                })
-                .UseStartup<Startup>();
+                });
 
-            this._testServer = new TestServer(webHostBuilder);
-            this._serviceScopeFactory = this._testServer.Services.GetService<IServiceScopeFactory>();
+            this._host = hostBuilder.Start();
+            this._serviceScopeFactory = this._host.Services.GetService<IServiceScopeFactory>();
+            
             this._jsonSerializerOptions = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true,
@@ -84,12 +91,12 @@ namespace DDDEfCore.ProductCatalog.WebApi.Tests
             }
         }
 
-        public async Task DoTest(Func<HttpClient, JsonSerializerOptions, IServiceProvider, Task> doTestFnc)
+        public async Task DoTest(Func<HttpClient, JsonSerializerOptions, Task> doTestFnc)
         {
-            using var client = this._testServer.CreateClient();
-            using var scope = this._serviceScopeFactory.CreateScope();
-            var serviceProvider = scope.ServiceProvider;
-            await doTestFnc(client, this._jsonSerializerOptions, serviceProvider);
+            using var client = this._host.GetTestClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            await doTestFnc(client, this._jsonSerializerOptions);
         }
 
         private async Task ResetCheckpoint()
