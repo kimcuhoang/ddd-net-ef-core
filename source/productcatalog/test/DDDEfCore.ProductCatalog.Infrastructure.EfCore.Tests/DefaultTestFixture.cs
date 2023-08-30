fbca764126510b064586e981ba7f8ae516ccc590
KimCH <kim.cuhoang@gmail.com>
2023-08-30 17:35:29 +0700

    Fix test repositories & remove redundant references

--- source/productcatalog/test/DDDEfCore.ProductCatalog.Infrastructure.EfCore.Tests/DefaultTestFixture.cs
+++ source/productcatalog/test/DDDEfCore.ProductCatalog.Infrastructure.EfCore.Tests/DefaultTestFixture.cs
@@ -6,125 +6,125 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json;
 using Xunit;
 
-namespace DDDEfCore.ProductCatalog.Infrastructure.EfCore.Tests
+namespace DDDEfCore.ProductCatalog.Infrastructure.EfCore.Tests;
+
+public class DefaultTestFixture : IAsyncLifetime
 {
-    public class DefaultTestFixture : IAsyncLifetime
+    public IFixture Fixture { get; }
+
+    protected readonly DefaultWebApplicationFactory _factory;
+
+    public DefaultTestFixture(DefaultWebApplicationFactory factory)
     {
-        public IFixture Fixture { get; }
+        this._factory = factory;
+        this.Fixture = new Fixture();
+    }
 
-        protected readonly DefaultWebApplicationFactory _factory;
+    #region Implementation of IAsyncLifetime
 
-        public DefaultTestFixture(DefaultWebApplicationFactory factory)
+    public virtual async Task InitializeAsync()
+    {
+        await this._factory.StartContainerAsync();
+
+        await this.ExecuteDbContextAsync(async dbContext =>
         {
-            this._factory = factory;
-            this.Fixture = new Fixture();
-        }
-
-        #region Implementation of IAsyncLifetime
-
-        public virtual async Task InitializeAsync()
-        {
-            await this._factory.StartContainerAsync();
-
-            await this.ExecuteDbContextAsync(async dbContext =>
+            var database = dbContext.Database;
+            var pendingMigrations = await database.GetPendingMigrationsAsync();
+            if (pendingMigrations.Any())
             {
-                var database = dbContext.Database;
-                var pendingMigrations = await database.GetPendingMigrationsAsync();
-                if (pendingMigrations.Any())
-                {
-                    await database.MigrateAsync();
-                }
-            });
-        }
+                await database.MigrateAsync();
+            }
+        });
+    }
 
-        public virtual async Task DisposeAsync() => await Task.Yield();
+    public virtual async Task DisposeAsync() => await Task.Yield();
 
-        public async Task ExecuteServiceAsync(Func<IServiceProvider, Task> func)
+    public async Task ExecuteServiceAsync(Func<IServiceProvider, Task> func)
+    {
+        await this._factory.ExecuteServiceAsync(func);
+    }
+
+    public async Task ExecuteTransactionDbContextAsync(Func<DbContext, Task> func)
+    {
+        await this._factory.ExecuteServiceAsync(async serviceProvider =>
         {
-            await this._factory.ExecuteServiceAsync(func);
-        }
+            var dbContext = serviceProvider.GetRequiredService<DbContext>();
 
-        public async Task ExecuteTransactionDbContextAsync(Func<DbContext, Task> func)
-        {
-            await this._factory.ExecuteServiceAsync(async serviceProvider =>
+            await using var transaction = await dbContext.Database.BeginTransactionAsync();
+            try
             {
-                var dbContext = serviceProvider.GetRequiredService<DbContext>();
-
-                await using var transaction = await dbContext.Database.BeginTransactionAsync();
-                try
-                {
-                    await func.Invoke(dbContext);
-                    await transaction.CommitAsync();
-                }
-                catch (Exception)
-                {
-                    await transaction.RollbackAsync();
-                    throw;
-                }
-            });
-        }
-
-        public async Task ExecuteDbContextAsync(Func<DbContext, Task> func)
-        {
-            await this._factory.ExecuteServiceAsync(async serviceProvider =>
-            {
-                var dbContext = serviceProvider.GetRequiredService<DbContext>();
-
                 await func.Invoke(dbContext);
-            });
-        }
-
-        public async Task ExecuteHttpClientAsync(Func<HttpClient, Task> func)
-        {
-            using var httpClient = this._factory.CreateClient();
-            await func.Invoke(httpClient);
-        }
-
-        public JsonSerializerOptions JsonSerializerOptions
-            => this._factory.JsonSerializerSettings;
-
-        public TModel? Parse<TModel>(string json) where TModel : class, new()
-        {
-            var model = default(TModel);
-
-            if (string.IsNullOrEmpty(json)) return model;
-
-            model = JsonSerializer.Deserialize<TModel>(json, this.JsonSerializerOptions);
-
-            return model;
-        }
-
-        #endregion
-
-        public async Task RepositoryExecute<TAggregate, TIdentity>(Func<IRepository<TAggregate, TIdentity>, Task> action) where TAggregate : AggregateRoot<TIdentity> where TIdentity : IdentityBase
-        {
-            await this.ExecuteServiceAsync(async serviceProvider =>
+                await transaction.CommitAsync();
+            }
+            catch (Exception)
             {
-                var repositoryFactory = serviceProvider.GetService<IRepositoryFactory>();
-                var repository = repositoryFactory.CreateRepository<TAggregate, TIdentity>();
-                await action(repository);
-            });
-        }
+                await transaction.RollbackAsync();
+                throw;
+            }
+        });
+    }
 
-        public async Task SeedingData<TAggregate, TIdentity>(params TAggregate[] entities) where TAggregate : AggregateRoot<TIdentity> where TIdentity : IdentityBase
+    public async Task ExecuteDbContextAsync(Func<DbContext, Task> func)
+    {
+        await this._factory.ExecuteServiceAsync(async serviceProvider =>
         {
-            if (entities == null || !entities.Any()) return;
+            var dbContext = serviceProvider.GetRequiredService<DbContext>();
 
-            await this.ExecuteDbContextAsync(async dbContext =>
+            await func.Invoke(dbContext);
+        });
+    }
+
+    public async Task ExecuteHttpClientAsync(Func<HttpClient, Task> func)
+    {
+        using var httpClient = this._factory.CreateClient();
+        await func.Invoke(httpClient);
+    }
+
+    public JsonSerializerOptions JsonSerializerOptions
+        => this._factory.JsonSerializerSettings;
+
+    public TModel? Parse<TModel>(string json) where TModel : class, new()
+    {
+        var model = default(TModel);
+
+        if (string.IsNullOrEmpty(json)) return model;
+
+        model = JsonSerializer.Deserialize<TModel>(json, this.JsonSerializerOptions);
+
+        return model;
+    }
+
+    #endregion
+
+    public async Task RepositoryExecute<TAggregate, TIdentity>(Func<IRepository<TAggregate, TIdentity>, Task> action) where TAggregate : AggregateRoot<TIdentity> where TIdentity : IdentityBase
+    {
+        await this.ExecuteServiceAsync(async serviceProvider =>
+        {
+            var repositoryFactory = serviceProvider.GetRequiredService<IRepositoryFactory>();
+            var repository = repositoryFactory.CreateRepository<TAggregate, TIdentity>();
+            await action(repository);
+            await repositoryFactory.Commit();
+        });
+    }
+
+    public async Task SeedingData<TAggregate, TIdentity>(params TAggregate[] entities) where TAggregate : AggregateRoot<TIdentity> where TIdentity : IdentityBase
+    {
+        if (entities == null || !entities.Any()) return;
+
+        await this.ExecuteDbContextAsync(async dbContext =>
+        {
+            await using var transaction = await dbContext.Database.BeginTransactionAsync();
+            try
             {
-                await using var transaction = await dbContext.Database.BeginTransactionAsync();
-                try
-                {
-                    await dbContext.Set<TAggregate>().AddRangeAsync(entities);
-                    await dbContext.SaveChangesAsync();
-                    await transaction.CommitAsync();
-                }
-                catch (Exception)
-                {
-                    await transaction.RollbackAsync();
-                    throw;
-                }
-            });
-        }
+                await dbContext.Set<TAggregate>().AddRangeAsync(entities);
+                await dbContext.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        });
     }
 }
